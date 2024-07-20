87cf8c37b538d987a3db9889c6bf838e4123ea23
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2024-07-20 16:14:13 +0700

    feat: method success register controller

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -69,4 +69,8 @@ class RegisterController extends Controller
             'password' => Hash::make($data['password']),
         ]);
     }
+
+    public function success(){
+        return view("auth.success");
+    }
 }
