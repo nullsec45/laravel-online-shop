10079bc121f5a1cf7cd2855b298db0c27743a5a8
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2024-07-28 13:20:55 +0700

    feat : layouts dashboard

---
+++ resources/views/layouts/dashboard.blade.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta
+      name="viewport"
+      content="width=device-width, initial-scale=1, shrink-to-fit=no"
+    />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+
+    <title>@yield('title')</title>
+
+    @include('includes.dashboard.style')
+  </head>
+
+  <body>
+    <div class="page-dashboard">
+      <div class="d-flex" id="wrapper" data-aos="fade-right">
+       {{-- sidebar --}}
+       @include('includes.dashboard.sidebar')
+
+        <!-- Page Content -->
+        <div id="page-content-wrapper">
+          <nav class="navbar navbar-expand-lg navbar-light navbar-store fixed-top" data-aos="fade-down">
+            <div class="container-fluid">
+              <button  class="btn btn-secondary d-md-none mr-auto mr-2" id="menu-toggle">
+                &laquo; Menu
+              </button>
+              {{-- navbar --}}
+              @include('includes.dashboard.navbar')
+            </div>
+          </nav>
+
+          {{-- Content --}}
+          @yield('content')
+
+        </div>
+      </div>
+    </div>
+
+    <!-- Bootstrap core JavaScript -->
+    @include('includes.dashboard.script')
+</html>
