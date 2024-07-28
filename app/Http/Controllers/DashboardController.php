32757034796097bef9eb06452905f07b18ed8f0f
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2024-07-28 13:19:11 +0700

    feat : new dashboard controller

---
+++ app/Http/Controllers/DashboardController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class DashboardController extends Controller
+{
+    public function index(){
+        $data=[
+            "transaction_data" => [
+                [
+                    'id' => 1,
+                    'product' => [
+                        'name' => 'Produk A',
+                        'galleries' => [
+                            ['photos' => 'images/product-card-1.png']
+                        ]
+                    ],
+                    'user' => [
+                        'name' => 'Pengguna A'
+                    ],
+                    'created_at' => '2023-07-28'
+                ],
+                [
+                    'id' => 2,
+                    'product' => [
+                        'name' => 'Produk B',
+                        'galleries' => [
+                            ['photos' => 'images/product-card-2.png']
+                        ]
+                    ],
+                    'user' => [
+                        'name' => 'Pengguna B'
+                    ],
+                    'created_at' => '2023-07-29'
+                ]
+            ]
+        ];
+
+        return view('pages.dashboard', $data);
+    }
+
+    public function transaction(string $id){
+        
+    }
+}
