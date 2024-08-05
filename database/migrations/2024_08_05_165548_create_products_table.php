0603103f83e6bab22b7eea7afa28fb5c4bb8199a
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2024-08-06 00:20:03 +0700

    feat : migration products

---
+++ database/migrations/2024_08_05_165548_create_products_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string("name");
+            $table->integer("users_id");
+            $table->unsignedBigInteger("categories_id");
+            $table->integer("price");
+            $table->longText("description");
+            $table->softDeletes();
+            $table->timestamps();
+
+            $table->foreign("categories_id")->on("categories")->references("id");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
