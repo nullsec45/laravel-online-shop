f45f006940c29559832602038d61400b65cb9a3d
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2024-08-07 21:49:47 +0700

    feat : new migration transaction & transaction detail

---
+++ database/migrations/2024_08_07_143708_create_transaction_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('transaction', function (Blueprint $table) {
+            $table->id();
+
+            $table->unsignedBigInteger('users_id');
+            $table->integer('insurance_price');
+            $table->integer('shipping_price');
+            $table->integer('transaction_status');
+            $table->string('resi');
+
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('transaction');
+    }
+};
