eeb4acdee1648850154c7825ec7f0b64075b2cd1
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2024-08-11 21:02:51 +0700

    feat : conditional sidebar admin or user

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -1,44 +1,47 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
+
+<head>
     <meta charset="utf-8" />
-    <meta
-      name="viewport"
-      content="width=device-width, initial-scale=1, shrink-to-fit=no"
-    />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
     <meta name="description" content="" />
     <meta name="author" content="" />
 
     <title>@yield('title')</title>
 
     @include('includes.dashboard.style')
-  </head>
+</head>
 
-  <body>
+<body>
     <div class="page-dashboard">
-      <div class="d-flex" id="wrapper" data-aos="fade-right">
-       {{-- sidebar --}}
-       @include('includes.dashboard.sidebar')
+        <div class="d-flex" id="wrapper" data-aos="fade-right">
+            {{-- sidebar --}}
+            @if($page == 'admin')
+            @include('includes.dashboard.sidebar-admin')
+            @else
+            @include('includes.dashboard.sidebar')
+            @endif
 
-        <!-- Page Content -->
-        <div id="page-content-wrapper">
-          <nav class="navbar navbar-expand-lg navbar-light navbar-store fixed-top" data-aos="fade-down">
-            <div class="container-fluid">
-              <button  class="btn btn-secondary d-md-none mr-auto mr-2" id="menu-toggle">
-                &laquo; Menu
-              </button>
-              {{-- navbar --}}
-              @include('includes.dashboard.navbar')
+            <!-- Page Content -->
+            <div id="page-content-wrapper">
+                <nav class="navbar navbar-expand-lg navbar-light navbar-store fixed-top" data-aos="fade-down">
+                    <div class="container-fluid">
+                        <button class="btn btn-secondary d-md-none mr-auto mr-2" id="menu-toggle">
+                            &laquo; Menu
+                        </button>
+                        {{-- navbar --}}
+                        @include('includes.dashboard.navbar')
+                    </div>
+                </nav>
+
+                {{-- Content --}}
+                @yield('content')
+
             </div>
-          </nav>
-
-          {{-- Content --}}
-          @yield('content')
-
         </div>
-      </div>
     </div>
 
     <!-- Bootstrap core JavaScript -->
     @include('includes.dashboard.script')
+
 </html>
