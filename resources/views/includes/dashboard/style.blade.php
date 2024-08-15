963cf98b8026683055cbc487f3a5b67a744cf461
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2024-08-15 22:09:53 +0700

    feat : import cdn datatables to view

--- resources/views/includes/dashboard/style.blade.php
+++ resources/views/includes/dashboard/style.blade.php
@@ -2,4 +2,5 @@
 @stack('prepend-style')
 <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet" />
 <link href="{{url('/style/main.css')}}" rel="stylesheet" />
+<link href="https://cdn.datatables.net/v/bs4/dt-2.1.3/datatables.min.css" rel="stylesheet">
 @stack('addon-style')
