665f9b90181c20017793bfc822d44a776ac9806b
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2024-08-17 11:41:03 +0700

    feat : route catetgories index admin

--- resources/views/includes/dashboard/sidebar-admin.blade.php
+++ resources/views/includes/dashboard/sidebar-admin.blade.php
@@ -1,18 +1,22 @@
  <!-- Sidebar -->
  <div class="border-right" id="sidebar-wrapper">
      <div class="sidebar-heading text-center">
-         <img src="{{url('/images/admin.png')}}" alt="" class="my-4 w-25" />
+         <!-- <img src="{{url('/images/admin.png')}}" alt="" class="my-4 img-fluid" /> -->
      </div>
      <div class="list-group list-group-flush">
-         <a href="{{ route('dashboard.index') }}"
-             class="list-group-item list-group-item-action {{ (request()->is('dashboard')) ? 'active' : '' }} ">
+         <a href="{{ route('admin.dashboard') }}"
+             class="list-group-item list-group-item-action {{ (request()->is('admin/dashboard')) ? 'active' : '' }} ">
              Dashboard
          </a>
          <a href="{{ route('dashboard.products') }}"
              class="list-group-item list-group-item-action {{ (request()->is('dashboard/products*')) ? 'active' : '' }} ">
-             My Products
+            Products
          </a>
-         <a href="{{ route('dashboard.transactions') }}"
+         <a href="{{ route('admin.categories.index') }}"
+             class="list-group-item list-group-item-action {{ (request()->is('admin/categories*')) ? 'active' : '' }} ">
+             Categories
+         </a>
+         <!-- <a href="{{ route('dashboard.transactions') }}"
              class="list-group-item list-group-item-action {{ (request()->is('dashboard/transactions*')) ? 'active' : '' }} ">
              Transactions
          </a>
@@ -26,6 +30,6 @@
          </a>
          <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
              @csrf
-         </form>
+         </form> -->
      </div>
  </div>
