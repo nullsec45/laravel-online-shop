aa1234adc3452c9301304b49f32563a6ac4a0969
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2024-08-17 11:40:10 +0700

    fix : fix typo library in

--- resources/views/includes/dashboard/script.blade.php
+++ resources/views/includes/dashboard/script.blade.php
@@ -1,6 +1,6 @@
 @stack('prepend-script')
 <script src="{{url('/vendor/jquery/jquery.min.js')}}"></script>
-<script src="{{url('/vendor/bootstrap/js/bootstrap.bundle.min.j')}}"></script>
+<script src="{{url('/vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
 <script src="https://cdn.datatables.net/v/bs4/dt-2.1.3/datatables.min.js"></script>
 <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
 <script>
