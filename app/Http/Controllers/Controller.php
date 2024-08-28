d22d84b6aa0dc4f0c13d7348119ed288929cc9b2
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2024-08-28 22:24:57 +0700

    feat/helpers: register helpres to BaseController.

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,11 +2,18 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
-use Illuminate\Foundation\Validation\ValidatesRequests;
+use App\Helpers\Helpers;
 use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
+
+    protected $helper;
+
+    public function __construct(){
+        $this->helper=new Helpers();
+    }
 }
