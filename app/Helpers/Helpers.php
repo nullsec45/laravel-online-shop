b9f030f1d3efc9874af2eada6c1f1db416496812
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2024-08-28 22:24:36 +0700

    feat/helpers: new helpers

---
+++ app/Helpers/Helpers.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Helpers;
+
+use Illuminate\Support\Facades\Crypt;
+use Illuminate\Support\Facades\Storage;
+class Helpers{
+    private function _hashFile(string $stringFile){
+        $encryptedString = Crypt::encryptString($stringFile);
+        $base64Encoded = base64_encode($encryptedString);
+        $safeString = preg_replace('/[^a-zA-Z0-9]/', '', $base64Encoded);
+        $desiredLength=10;
+        $finalString = substr($safeString, 0, $desiredLength);
+
+        return $finalString;
+    }
+
+     public function fileUploadHandling($requestFile, $prefixName, $path, $typeUpload, $fileOld=null){
+        $file=$requestFile;
+        
+        if($typeUpload == "update"){
+            $storage=Storage::disk('public');
+            $fileOld=$path."/".$fileOld;
+            if ($storage->exists($fileOld)) {
+                $storage->delete($fileOld);
+            }
+        }
+
+        // untuk prefixName itu awalan nama file nya dan kalau pake spasi sambungkan pake undersorce dan jangan lebh dari 10 karakter
+        $fileName=$prefixName."_".$this->_hashFile($file->getClientOriginalName())."_".time().".".$file->getClientOriginalExtension();
+        $file->storeAs($path, $fileName, "public");
+
+        return $fileName;
+    }
+}
