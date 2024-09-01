02f25e84c93cffbe270fa5cbe83dce391bdbbd68
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2024-09-01 21:14:30 +0700

    feat/product-gallery : new view create & index.

---
+++ resources/views/pages/admin/product-galleries/create.blade.php
@@ -0,0 +1,93 @@
+@extends('layouts.dashboard')
+
+@section('title')
+    Product Galleries
+@endsection
+
+@section('content')
+<div class="section-content section-dashboard-home" data-aos="fade-up">
+    <div class="container-fluid">
+        <div class="dashboard-heading">
+            <h2 class="dashboard-title">Product Galleries</h2>
+            <p class="dashboard-subtitle">
+                Upload Image Product
+            </p>
+        </div>
+        <div class="dashboard-content">
+            <div class="row">
+                <div class="col-md-12">
+                    @if($errors->any())
+                        <div class="alert alert-danger">
+                            <ul>
+                                @foreach($errors->all() as $error)
+                                    <li>{{$error}}</li>
+                                @endforeach
+                            </ul>
+                        </div> 
+                    @endif
+                    <div class="card">
+                        <div class="card-body">
+                           <form action="{{route('admin.products.store')}}" method="POST" enctype="multipart/form-data">
+                                @csrf
+                                <div class="row">
+                                    <div class="col-md-12">
+                                        <div class="form-group">
+                                            <label for="product_name">Product Name</label>
+                                            <input type="text" name="name" id="product_name" class="form-control" required>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-12">
+                                        <div class="form-group">
+                                            <label for="users_id">Product Owner</label>
+                                             <select name="users_id" id="users_id" class="form-control">
+                                                @foreach ($users as $user )
+                                                    <option value="{{$user->id}}">{{$user->name}}</option>
+                                                @endforeach
+                                            </select>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-12">
+                                        <div class="form-group">
+                                            <label for="categories_id">Product Category</label>
+                                             <select name="categories_id" id="categories_id" class="form-control">
+                                                @foreach ($categories as $category )
+                                                    <option value="{{$category->id}}">{{$category->name}}</option>
+                                                @endforeach
+                                            </select>
+                                        </div>
+                                    </div>
+                                   <div class="col-md-12">
+                                        <div class="form-group">
+                                            <label for="price">Price</label>
+                                            <input type="number" name="price" id="price" class="form-control" required>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-12">
+                                        <div class="form-group">
+                                            <label for="editor">Product Description</label>
+                                            <textarea name="description" id="editor" class="form-control" cols="3" rows="3"></textarea>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col text-right">
+                                        <button type="submit" class="btn btn-success px-5">Save Now</button>
+                                    </div>
+                                </div>
+                           </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+@endsection
+
+@push('addon-script')
+<script src="https://cdn.ckeditor.com/4.14.0/standard/ckeditor.js"></script>
+<script>
+CKEDITOR.replace("editor");
+</script>
+@endpush
---
+++ resources/views/pages/admin/product-galleries/index.blade.php
@@ -0,0 +1,73 @@
+@extends('layouts.dashboard')
+
+@section('title')
+    Product Galleries
+@endsection
+
+@section('content')
+<div class="section-content section-dashboard-home" data-aos="fade-up">
+    <div class="container-fluid">
+        <div class="dashboard-heading">
+            <h2 class="dashboard-title">Galleries</h2>
+            <p class="dashboard-subtitle">
+                List of Galleries
+            </p>
+        </div>
+        <div class="dashboard-content">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="card">
+                        <div class="card-body">
+                            <div class="container-fluid">
+                             <a href="{{route('admin.products.create')}}" class="btn btn-primary float-right mb-4">Add Product</a>
+                            </div>
+                            <div class="table table-responsive mt-4">
+                                <table class="table-hover scroll-horizontal-vertical w-100 mt-4" id="table-products">
+                                    <thead>
+                                        <tr>
+                                            <th>ID</th>
+                                            <th>Product</th>
+                                            <th>Photo</th>
+                                            <th>Action</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody></tbody>
+                                </table>
+                            </div>
+                        </div>
+                       
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
+
+@push('addon-script')
+    <script>
+        let datatable=$("#table-products").DataTable({
+            processing:true,
+            serverSide:true,
+            ordering:true,
+            ajax:{
+                url:"{!! url()->current() !!}"
+            },
+            columns:[
+                {data:"id", name:"id"},
+                {data:"product.name", name:"product.name"},
+                {data:"photos", name:"photos"},
+                {
+                 data:"actions", 
+                 name:"actions", 
+                 orderable:false,
+                 searchable:false, 
+                 width:"15%",
+                 render: function (data, type, row, meta){
+                    return data;
+                 }
+                },
+            ]
+        });
+    </script>
+@endpush
