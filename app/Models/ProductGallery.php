5f70a17f4a1ad4201f8852ad78d9a84fa363a6cf
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2024-09-01 21:13:32 +0700

    feat/product-gallery: add fillable, hidden, and realtion product in model

--- app/Models/ProductGallery.php
+++ app/Models/ProductGallery.php
@@ -8,4 +8,18 @@ use Illuminate\Database\Eloquent\Model;
 class ProductGallery extends Model
 {
     use HasFactory;
+
+     protected $fillable=[
+        "photos",
+        "products_id",
+    ];
+
+    protected $hidden=[
+        "created_at",
+        "updated_at"
+    ];
+
+     public function product(){
+        return $this->belongsTo(Product::class, "products_id","id");
+    }
 }
