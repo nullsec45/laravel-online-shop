6269907ff8af066d5ea289bba7bba32ec9d07a31
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2024-09-08 14:41:28 +0700

    feat/product-gallery : change import product gallery controller

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,12 @@
 <?php
 
-use App\Http\Controllers\ProductGalleryController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\{
     DashboardController as AdminDashboardController, 
     CategoryController as AdminCategoryController,
     UserController,
-    ProductController
+    ProductController,
+    ProductGalleryController
 };
 
 use App\Http\Controllers\{
