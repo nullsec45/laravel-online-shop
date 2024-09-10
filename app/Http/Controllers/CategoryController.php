0b7922288d66be08c24c8d914f9666012ef62ce8
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2024-09-10 23:54:18 +0700

    feat/category : dinamis data index controller

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -2,94 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
+use App\Models\Category;
 use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
     public function index(){
         $data=[
-            "categories" => [
-                [
-                    "name" => "Gadgets",
-                    "icon" => "categories-gadgets.svg",
-                    "slug" => "gadgets"
-                ],
-                [
-                    "name" => "Furniture",
-                    "icon" => "categories-furniture.svg",
-                    "slug" => "furniture"
-                ],
-                [
-                    "name" => "Makeup",
-                    "icon" => "categories-makeup.svg",
-                    "slug" => "makeup"
-                ],
-                [
-                    "name" => "Sneaker",
-                    "icon" => "categories-sneaker.svg",
-                    "slug" => "sneaker"
-                ],
-                [
-                    "name" => "Tools",
-                    "icon" => "categories-tools.svg",
-                    "slug" => "tools"
-                ],
-                [
-                    "name" => "Baby",
-                    "icon" => "categories-baby.svg",
-                    "slug" => "baby"
-                ]
-            ],
-            "products" => [
-                [
-                    "name" => "Apple Watch 4",
-                    "price" => "890",
-                    "photo" => "products-apple-watch.jpg",
-                    "slug" => "apple-watch-4"
-                ],
-                [
-                    "name" => "Orange Bogotta",
-                    "price" => "94,509",
-                    "photo" => "products-orange-bogotta.jpg",
-                    "slug" => "orange-boggota"
-                ],
-                [
-                    "name" => "Sofa Ternyaman",
-                    "price" => "1,409",
-                    "photo" => "products-sofa-ternyaman.jpg",
-                    "slug" => "sofa-ternyaman"
-                ],
-                [
-                    "name" => "Bubuk Maketti",
-                    "price" => "225",
-                    "photo" => "products-bubuk-maketti.jpg",
-                    "slug" => "bubuk-maketti"
-                ],
-                [
-                    "name" => "Tatakan Gelas",
-                    "price" => "45,184",
-                    "photo" => "products-tatakan-gelas.jpg",
-                    "slug" => "tatakan-gelas"
-                ],
-                [
-                    "name" => "Mavic Kawe",
-                    "price" => "503",
-                    "photo" => "products-mavic-kawe.jpg",
-                    "slug" => "mavic-kawe"
-                ],
-                [
-                    "name" => "Black Edition Nike",
-                    "price" => "70,402",
-                    "photo" => "products-black-edition-nike.jpg",
-                    "slug" => "black-edition-nike"
-                ],
-                [
-                    "name" => "Monkey Toys",
-                    "price" => "783",
-                    "photo" => "products-monkey-toys.jpg",
-                    "slug" => "monkey-toys"
-                ]
-            ]
+            "categories" => Category::take(6)->get(),
+            "products" => Product::with("galleries")->paginate(10)
         ];
 
         return view("pages.category", $data);
@@ -99,7 +21,19 @@ class CategoryController extends Controller
     
     public function store(){}
     
-    public function show(){}
+    public function show(string $slug){
+
+        $categories=Category::all();
+        $category_id=Category::where("slug", $slug)->firstOrFail()->id;
+        $products=Product::with("galleries")->where("categories_id", $category_id)->paginate(10);
+
+        $data=[
+            "categories" => $categories,
+            "products" => $products
+        ];
+
+        return view("pages.category", $data);
+    }
 
     public function edit(){}
 
