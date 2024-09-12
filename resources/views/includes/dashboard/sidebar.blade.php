bae60e7d7d74fcb3dc7d5fb6ac59627971e25e4b
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2024-09-12 22:39:46 +0700

    feat/sidebar : fix route sidebar.

--- resources/views/includes/dashboard/sidebar.blade.php
+++ resources/views/includes/dashboard/sidebar.blade.php
@@ -11,26 +11,26 @@
         Dashboard
       </a>
       <a
-        href="{{ route('dashboard.products') }}"
+        href="{{ route('dashboard.products.index') }}"
         class="list-group-item list-group-item-action {{ (request()->is('dashboard/products*')) ? 'active' : '' }} "
       >
         My Products
       </a>
       <a
-        href="{{ route('dashboard.transactions') }}"
+        href="{{ route('dashboard.transactions.index') }}"
         class="list-group-item list-group-item-action {{ (request()->is('dashboard/transactions*')) ? 'active' : '' }} "
       >
         Transactions
       </a>
       <a
-        href="{{ route('dashboard.settings-store') }}"
-        class="list-group-item list-group-item-action {{ (request()->is('dashboard/settings*')) ? 'active' : '' }} "
+        href="{{ route('dashboard.settings.store') }}"
+        class="list-group-item list-group-item-action {{ (request()->is('dashboard/settings/store')) ? 'active' : '' }} "
       >
         Store Settings
       </a>
       <a
-        href="{{ route('dashboard.settings-account') }}"
-        class="list-group-item list-group-item-action {{ (request()->is('dashboard/account*')) ? 'active' : '' }} "
+        href="{{ route('dashboard.settings.account') }}"
+        class="list-group-item list-group-item-action {{ (request()->is('dashboard/settings/account')) ? 'active' : '' }} "
       >
         My Account
       </a>
