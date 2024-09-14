958a7754166bcbfb3c2b8c2bea85d2b8c9e1aca4
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2024-09-14 14:06:06 +0700

    feat/register : add column in fillable.

--- app/Models/User.php
+++ app/Models/User.php
@@ -21,7 +21,9 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
-        'roles'
+        'roles',
+        'categories_id',
+        'store_status'
     ];
 
     /**
