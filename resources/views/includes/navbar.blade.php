d263a26baa1e96ffe46fef84de882f2591f7d85a
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2024-09-20 21:23:28 +0700

    feat/cart : count total cart in navbar.

--- resources/views/includes/navbar.blade.php
+++ resources/views/includes/navbar.blade.php
@@ -73,9 +73,18 @@
                     </div>
                 </li>
                 <li class="nav-item">
-                <a href="#" class="nav-link d-inline-block mt-2">
-                    <img src="{{url('/images/icon-cart-empty.svg')}}" alt="" />
-                </a>
+                    <a href="#" class="nav-link d-inline-block mt-2">
+                        @php
+                            $carts=\App\Models\Cart::where("users_id", Auth::user()->id)->count();
+                        @endphp
+
+                        @if($carts > 0)
+                             <img src="{{url('/images/icon-cart-filled.svg')}}" alt="" />
+                             <div class="card-badge">{{$carts}}</div>
+                        @else
+                             <img src="{{url('/images/icon-cart-empty.svg')}}" alt="" />
+                        @endif
+                    </a>
                 </li>
             </ul>
             @endauth
