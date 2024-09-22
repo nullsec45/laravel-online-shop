83397d61343f494462b872746c35732ad9b3f262
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2024-09-22 22:00:55 +0700

    feat/sidebar L remove sidebar logout.

--- resources/views/includes/dashboard/sidebar.blade.php
+++ resources/views/includes/dashboard/sidebar.blade.php
@@ -34,16 +34,5 @@
       >
         My Account
       </a>
-      <a
-         href="{{ route('logout') }}"
-         onclick="event.preventDefault();
-                  document.getElementById('logout-form').submit();"
-         class="list-group-item list-group-item-action"
-      >
-        Sign Out
-      </a>
-      <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-          @csrf
-      </form>
     </div>
   </div>
