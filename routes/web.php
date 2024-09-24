1424f74edfec14613df2ebf867f5921e94ecc996
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2024-09-25 00:58:55 +0700

    chore(middleware) : implements middleware to all routes.

--- routes/web.php
+++ routes/web.php
@@ -41,10 +41,8 @@ use App\Http\Controllers\Auth\RegisterController;
 
 Auth::routes();
 
+// Global
 Route::get("/", [HomeController::class, "index"])->name("home");
-
-// Route::get("/categories", [CategoryController::class, "index"])->name("categories");
-// Route::get("/categories-detail", [CategoryController::class, "show"])->name("categories-detail");
 
 Route::get("categories",[CategoryController::class,"index"])->name("categories.index");
 Route::get("categories/{slug}",[CategoryController::class,"show"])->name("categories.show");
@@ -59,31 +57,38 @@ Route::post("/checkout/callback", [CheckoutController::class, "callback"])->name
 
 Route::get("/success", [CartController::class, "success"])->name("checkout-success");
 Route::get("/register/success", [RegisterController::class, "success"])->name("register-success");
-
-Route::get("/dashboard", [DashboardController::class,"index"])->name("dashboard.index");
-
-Route::prefix("dashboard")->name("dashboard.")->group(function(){
-    Route::resource("products",DashboardProductController::class);
-    Route::resource("transactions",DashboardTransactionController::class);
-    Route::prefix("settings")->name("settings.")
-            ->controller(DashboardSettingController::class)
-            ->group(function(){
-        Route::get("account","account")->name("account");
-        Route::put("account/update/{redirect}","update")->name("account-update");
-        Route::get("store","store")->name("store");
-        Route::put("store/update/{redirect}","update")->name("store-update");
-    });
-});
-
-Route::prefix("admin")->name("admin.")->group(function(){
-    Route::get("dashboard",[AdminDashboardController::class,"index"])->name("dashboard");
-    Route::resource("categories", AdminCategoryController::class);
-    Route::resource("users",UserController::class);
-    Route::resource("products",ProductController::class);
-    Route::resource("product-galleries",ProductGalleryController::class);
-});
-
 Route::get("register/check", [RegisterController::class,"check"])->name("api-register-check");
 
 Route::get("provinces", [LocationControlller::class, "provinces"])->name("api-provinces");
 Route::get("regencies/{province_id}", [LocationControlller::class, "regencies"])->name("api-regencies");
+
+// Admin
+Route::group(["middleware" => "admin"], function(){
+    Route::prefix("admin")->name("admin.")->group(function(){
+        Route::get("dashboard",[AdminDashboardController::class,"index"])->name("dashboard");
+        Route::resource("categories", AdminCategoryController::class);
+        Route::resource("users",UserController::class);
+        Route::resource("products",ProductController::class);
+        Route::resource("product-galleries",ProductGalleryController::class);
+    });
+});
+
+// User
+Route::group(["middleware" => "user"], function(){
+    Route::get("/dashboard", [DashboardController::class,"index"])->name("dashboard.index");
+    Route::prefix("dashboard")->name("dashboard.")->group(function(){
+        Route::resource("products",DashboardProductController::class);
+        Route::resource("transactions",DashboardTransactionController::class);
+        Route::prefix("settings")->name("settings.")
+                ->controller(DashboardSettingController::class)
+                ->group(function(){
+            Route::get("account","account")->name("account");
+            Route::put("account/update/{redirect}","update")->name("account-update");
+            Route::get("store","store")->name("store");
+            Route::put("store/update/{redirect}","update")->name("store-update");
+        });
+    });
+});
+
+
+
