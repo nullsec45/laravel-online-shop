b0e8e055decda9b8afa7956b09b08fa21f3a84c7
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2024-10-05 20:46:11 +0700

    feat/dashboard-products : change call data from db, errors element,  delete image in detail view.

--- resources/views/pages/dashboard-products-detail.blade.php
+++ resources/views/pages/dashboard-products-detail.blade.php
@@ -16,61 +16,61 @@
         <div class="dashboard-content">
             <div class="row">
                 <div class="col-12">
-                    @if (!empty($errors))
+                    @if ($errors->any())
                     <div class="alert alert-danger">
                         <ul>
-                            @foreach ($errors as $error)
+                            @foreach ($errors->all() as $error)
                             <li>{{ $error }}</li>
                             @endforeach
                         </ul>
                     </div>
                     @endif
-                    <form action="{{ route('dashboard.products.update', $product['id']) }}" method="POST"
+                    <form action="{{ route('dashboard.products.update', $product->id) }}" method="POST"
                         enctype="multipart/form-data">
-                        @csrf
                         @method('PUT')
-                        <input type="hidden" name="users_id" value="1">
+                        @csrf
                         <div class="card">
                             <div class="card-body">
                                 <div class="row">
                                     <div class="col-md-6">
                                         <div class="form-group">
-                                            <label>Nama Produk</label>
+                                            <label>Product Name</label>
                                             <input type="text" name="name" class="form-control"
-                                                value="{{ $product['name'] }}" />
+                                                value="{{ $product->name ?? '-' }}" />
                                         </div>
                                     </div>
                                     <div class="col-md-6">
                                         <div class="form-group">
-                                            <label>Harga</label>
+                                            <label>Price</label>
                                             <input type="number" name="price" class="form-control"
-                                                value="{{ $product['price'] }}" />
+                                                value="{{ $product->price ?? '-' }}" />
                                         </div>
                                     </div>
                                     <div class="col-md-12">
                                         <div class="form-group">
-                                            <label>Kategori</label>
+                                            <label>Category</label>
                                             <select name="categories_id" class="form-control">
-                                                <option value="{{ $product['categories_id'] }}">Tidak diganti
-                                                    ({{ $product['category']['name'] }})</option>
+                                                <option value="{{ $product->categories_id }}">
+                                                    {{ $product->category->name }}
+                                                </option>
                                                 @foreach ($categories as $category)
-                                                <option value="{{ $category['id'] }}">{{ $category['name'] }}</option>
+                                                      <option value="{{ $category->id }}">{{ $category->name }}</option>
                                                 @endforeach
                                             </select>
                                         </div>
                                     </div>
                                     <div class="col-md-12">
                                         <div class="form-group">
-                                            <label>Deskripsi</label>
+                                            <label>Description</label>
                                             <textarea name="description"
-                                                id="editor">{{ $product['description'] }}</textarea>
+                                                id="editor">{{ $product->description }}</textarea>
                                         </div>
                                     </div>
                                 </div>
                                 <div class="row">
                                     <div class="col text-right">
                                         <button type="submit" class="btn btn-success px-5 btn-block">
-                                            Simpan Sekarang
+                                            Save Now
                                         </button>
                                     </div>
                                 </div>
@@ -84,27 +84,32 @@
                     <div class="card">
                         <div class="card-body">
                             <div class="row">
-                                @foreach ($product['galleries'] as $gallery)
-                                <div class="col-md-4">
-                                    <div class="gallery-container">
-                                        <img src="{{url("/images/".$gallery['photos']) }}" alt="" class="w-100" />
-                                        <a href=""
-                                            class="delete-gallery">
-                                            <img src="{{url('/images/icon-delete.svg')}}" alt="" />
-                                        </a>
+                               @foreach ($product->galleries as $gallery)
+                                    <div class="col-md-4">
+                                        <div class="gallery-container">
+                                            <img src="{{url("storage/assets/products/".$gallery->photos)}}" alt="" class="w-100" />
+                                            <a href="{{route('dashboard.products-gallery-delete', $gallery->id)}}"
+                                                class="delete-gallery"
+                                                onclick="event.preventDefault(); this.nextElementSibling.submit();">
+                                                <img src="{{url('/images/icon-delete.svg')}}" alt="" />
+                                            </a>
+                                            <form action="{{ route('dashboard.products-gallery-delete', $gallery->id) }}" method="POST" style="display: none;">
+                                                @csrf
+                                                @method("DELETE")
+                                            </form>
+                                        </div>
                                     </div>
-                                </div>
                                 @endforeach
                                 <div class="col-12">
-                                    <form action="" method="POST"
+                                    <form action="{{route('dashboard.products-gallery-upload')}}" method="POST"
                                         enctype="multipart/form-data">
                                         @csrf
-                                        <input type="hidden" value="{{ $product['id'] }}" name="products_id">
+                                        <input type="hidden" value="{{ $product->id }}" name="products_id">
                                         <input type="file" name="photos" id="file" style="display: none;" multiple
                                             onchange="form.submit()" />
                                         <button type="button" class="btn btn-secondary btn-block mt-3"
                                             onclick="thisFileUpload()">
-                                            Tambah Foto
+                                            Add Photo
                                         </button>
                                     </form>
                                 </div>
