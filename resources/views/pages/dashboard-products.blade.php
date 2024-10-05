c01396841105ce75752da6e02760e5cc87965e19
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2024-10-05 20:46:36 +0700

    feat/dsahboard-products : change path storage image in view.

--- resources/views/pages/dashboard-products.blade.php
+++ resources/views/pages/dashboard-products.blade.php
@@ -21,14 +21,15 @@
             </div>
             <div class="row mt-4">
                 @foreach ($products as $product)
+               
                 <div class="col-12 col-sm-6 col-md-4 col-lg-3">
                     <a href="{{ route('dashboard.products.show', $product['id']) }}"
                         class="card card-dashboard-product d-block">
                         <div class="card-body">
-                            <img src="{{ asset($product['galleries'][0]['photos']) }}" alt=""
+                            <img src="{{ asset('storage/assets/products/'.$product->galleries->first()->photos ?? '') }}" alt=""
                                 class="w-100 mb-2" />
-                            <div class="product-title">{{ $product['name'] }}</div>
-                            <div class="product-category">{{ $product['category']['name'] }}</div>
+                            <div class="product-title">{{ $product->name}}</div>
+                            <div class="product-category">{{ $product->category->name }}</div>
                         </div>
                     </a>
                 </div>
