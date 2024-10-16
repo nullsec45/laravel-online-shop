98424b3443407a2dd5f6bb061a3060b4a7320ce4
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2024-10-16 22:18:57 +0700

    feat/dashbioard-settings : change get data from array to stdObject.

--- resources/views/pages/dashboard-settings.blade.php
+++ resources/views/pages/dashboard-settings.blade.php
@@ -3,8 +3,8 @@
 @section('title')
     Store Settings
 @endsection
-
 @section('content')
+
 <div class="section-content section-dashboard-home" data-aos="fade-up">
     <div class="container-fluid">
         <div class="dashboard-heading">
@@ -27,16 +27,16 @@
                                         <div class="form-group">
                                             <label>Nama Toko</label>
                                             <input type="text" class="form-control" name="store_name"
-                                                   value="{{ $user['store_name'] }}" />
+                                                   value="{{ $user->store_name }}" />
                                         </div>
                                     </div>
                                     <div class="col-md-6">
                                         <div class="form-group">
                                             <label>Kategori</label>
                                             <select name="categories_id" class="form-control">
-                                                <option value="{{ $user['categories_id'] }}">Tidak diganti</option>
+                                                <option value="{{ $user->categories_id}}">Tidak diganti</option>
                                                 @foreach ($categories as $category)
-                                                    <option value="{{ $category['id'] }}">{{ $category['name'] }}</option>
+                                                    <option value="{{ $category->id }}">{{ $category->name }}</option>
                                                 @endforeach
                                             </select>
                                         </div>
@@ -50,7 +50,7 @@
                                             <div class="custom-control custom-radio custom-control-inline">
                                                 <input type="radio" class="custom-control-input" name="store_status"
                                                        id="openStoreTrue" value="1"
-                                                       {{ $user['store_status'] == 1 ? 'checked' : '' }} />
+                                                       {{ $user->store_status == 1 ? 'checked' : '' }} />
                                                 <label for="openStoreTrue" class="custom-control-label">
                                                     Buka
                                                 </label>
@@ -58,7 +58,7 @@
                                             <div class="custom-control custom-radio custom-control-inline">
                                                 <input type="radio" class="custom-control-input" name="store_status"
                                                        id="openStoreFalse" value="0"
-                                                       {{ $user['store_status'] == 0 || $user['store_status'] == NULL ? 'checked' : '' }} />
+                                                       {{ $user->store_status == 0 || $user->store_status == NULL ? 'checked' : '' }} />
                                                 <label for="openStoreFalse" class="custom-control-label">
                                                     Sementara Tutup
                                                 </label>
