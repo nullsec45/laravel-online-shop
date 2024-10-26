8abe7a5b48ec27eceb099398dbfa5fcb26e42d38
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2024-10-26 23:15:04 +0700

    feat : language message validation set to english.

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,6 +6,8 @@ use App\Helpers\Helpers;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Support\Facades\App;
+
 
 class Controller extends BaseController
 {
@@ -14,6 +16,7 @@ class Controller extends BaseController
     protected $helper;
 
     public function __construct(){
+        App::setLocale("en");     
         $this->helper=new Helpers();
     }
 }
