e06eef5d59a38456f684e93c0ea4c381f83d4a38
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2024-11-04 22:39:54 +0700

    feat/review : add style for star ratings.

--- resources/views/includes/style.blade.php
+++ resources/views/includes/style.blade.php
@@ -1,2 +1,8 @@
 <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet" />
 <link href="{{url("/style/main.css")}}" rel="stylesheet" />
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<style>
+    .checked {
+        color: orange;
+    }
+</style>
