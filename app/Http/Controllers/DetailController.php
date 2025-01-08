78af9654eb941fdb002ea201b1ab65b4d725663f
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2025-01-08 23:25:20 +0700

    feat/review : get data product reviews in detail controller

--- app/Http/Controllers/DetailController.php
+++ app/Http/Controllers/DetailController.php
@@ -4,12 +4,26 @@ namespace App\Http\Controllers;
 
 use App\Models\Product;
 use Illuminate\Http\Request;
+use App\Models\ProductReview;
 
 class DetailController extends Controller
 {
     public function ShowProduct(Request $request, string $slug){
         $product=Product::with(["galleries","user"])->where("slug",$slug)->firstOrFail();
+        $reviews= ProductReview::with(['comment.user'])
+                                ->where('products_id', $product->id)
+                                ->get()
+                                ->map(function ($review) {
+                                    return [
+                                        'user_name' => $review->comment->user->name ?? 'Unknown User',
+                                        'comment' => $review->comment->comment ?? 'No Comment'
+                                    ];
+                                });
+                                
         
-        return view("pages.detail", compact("product"));
+        return view("pages.detail", [
+            "product" => $product,
+            "reviews" => $reviews
+        ]);
     }
 }
