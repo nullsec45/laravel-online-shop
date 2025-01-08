1f4c78f4cbb37a9011f3cc0c13a834df5643f8d0
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2025-01-08 23:27:29 +0700

    feat/review : migration add column user id in table comments.

---
+++ database/migrations/2025_01_05_093102_add_column_to_comments_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('product_comments', function (Blueprint $table) {
+            $table->unsignedBigInteger("users_id")->after("comment")->nullable();
+
+            $table->foreign("users_id")->on("users")->references("id");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('comments', function (Blueprint $table) {
+            //
+        });
+    }
+};
