38a310e01f60938e7efccf3cd109f057a838ef8a
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2025-01-08 23:25:54 +0700

    feat/review : add method store

--- app/Http/Controllers/ReviewController.php
+++ app/Http/Controllers/ReviewController.php
@@ -2,9 +2,26 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Comment;
 use Illuminate\Http\Request;
+use App\Models\ProductReview;
+use Illuminate\Support\Facades\Auth;
 
 class ReviewController extends Controller
 {
-    //
+    public function store(string $id, Request $request){
+        $comment=Comment::create([
+            "comment" => $request->product_reviews,
+            "users_id" => Auth::user()->id,
+        ]);
+
+        ProductReview::create(
+            [
+                "comments_id" => $comment->id,
+                "products_id" => $id
+            ]
+        );
+
+        return redirect()->back();
+    }
 }
